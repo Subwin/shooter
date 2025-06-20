d33f9778759a6452b692fe46faa356517a074d68
yangyaxiong <281505235@qq.com>
2025-06-20 15:12:44 +0800

    feat(scenes): add drone enemy and update level layout
    - Add drone enemy character with basic movement
    - Update level layout to include bed, bill board, and drone
    - Modify player character to use CharacterBody2D and move_and_slide
    - Add collision shapes to objects and player

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,8 +1,9 @@
-extends Node2D
+extends CharacterBody2D
 
 func _process(delta: float) -> void:
     var dirction = Input.get_vector("left", "right", "up", "down")
-    position += dirction * 600 * delta
+    velocity = dirction * 600
+    move_and_slide()
     
     if Input.is_action_pressed("primary action"):
         print("shoot")
